262da9dbe58d1f1e9de7d4ed0e7cd31bef6d7713
Wilson Liu <wliu5@unimelb.edu.au>
2018-03-15 11:57:31 +1100

    download aterm.jar if not exist

--- windows/aterm-import.cmd
+++ windows/aterm-import.cmd
@@ -1,19 +1,37 @@
 @echo off
 
 REM check if java exists
-
 WHERE java >NUL 2>NUL
 IF %ERRORLEVEL% NEQ 0 (
-    ECHO Error: cannot find java. && EXIT /B 1
+    ECHO cannot find java. && EXIT /B 1
 )
 
-REM check if aterm.jar exists: aterm.jar should be in the same directory with this script. Otherwise, specify its path like below:
-REM SET MFLUX_ATERM=c:\mediaflux\bin\aterm.jar
 
+REM check if aterm.jar. If not, it will try download from the link below
 SET MFLUX_ATERM=%~dp0aterm.jar
+SET ATERM_DOWNLOAD_LINK=https://mediaflux.vicnode.org.au/mflux/aterm.jar
+
+REM download with POWERSHELL
+IF NOT EXIST %MFLUX_ATERM% (
+    WHERE POWERSHELL >NUL 2>NUL
+    IF %ERRORLEVEL% EQU 0 (
+        POWERSHELL -COMMAND "(New-Object Net.WebClient).DownloadFile('%ATERM_DOWNLOAD_LINK%', '%MFLUX_ATERM%')" >NUL 2>NUL
+    )
+)
+
+REM download with BITSADMIN
+IF NOT EXIST %MFLUX_ATERM% (
+    WHERE BITSADMIN >NUL 2>NUL
+    IF %ERRORLEVEL% EQU 0 (
+    	BITSADMIN /TRANSFER "Download aterm.jar" %ATERM_DOWNLOAD_LINK% %MFLUX_ATERM% >NUL 2>NUL
+    )
+)
+
+REM failed to download aterm.jar, exit
 IF NOT EXIST %MFLUX_ATERM% (
     ECHO cannot find %MFLUX_ATERM%. && EXIT /B 1
 )
+
 
 REM check if mflux.cfg exists
 SET MFLUX_CFG=%~dp0mflux.cfg
@@ -25,11 +43,11 @@ IF NOT EXIST %MFLUX_CFG% (
 )
 
 REM show usage if no argument or the 1st argument is -h, --help or /h
-SET HELP F
-IF [%1]==[]       SET HELP T
-IF "%1"=="/h"     SET HELP T
-IF "%1"=="-h"     SET HELP T
-IF "%1"=="--help" SET HELP T
+SET HELP=F
+IF [%1]==[]       SET HELP=T
+IF "%1"=="/h"     SET HELP=T
+IF "%1"=="-h"     SET HELP=T
+IF "%1"=="--help" SET HELP=T
 IF %HELP%==T (
     java -jar -Dmf.cfg=%MFLUX_CFG% %MFLUX_ATERM% nogui help import && EXIT /B 0
 )
